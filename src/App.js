e3a2847a256e1c3b23ed7e92da3d0936cae1c060
andrea-maiolo <maiolo.m.2@gmail.com>
2022-03-20 16:02:19 +0100

    deleted decoding in question component, changed fetch url, created answersArray in app componen

--- src/App.js
+++ src/App.js
@@ -10,18 +10,20 @@ function App() {
   const [formData, setFormData] = React.useState({})
 
   React.useEffect(()=>{
-    console.log("fetching")
-      fetch("https://opentdb.com/api.php?amount=5&encode=url3986")
+      fetch("https://opentdb.com/api.php?amount=5")
       .then(response => response.json())
       .then(data => setFetchQuestions(data.results))
   },[])
 
   React.useEffect(()=>{
     setId()
-    decodeQuestion()
   },[fetchQuestions])
 
-  function setId(){
+  React.useEffect(()=>{
+    decoding()
+  },[allQuestions])
+
+  const setId = function (){
     if(fetchQuestions.length != 0 ){
       const updateQuestions = fetchQuestions.map(question =>{
         return {
@@ -30,97 +32,84 @@ function App() {
         }
       })
     setAllQuestions(updateQuestions)
-    console.log("questions ready")
     }
   }
 
   //new code
 
-  function decodeQuestion(){
-    console.log("running decoding")
-    if(fetchQuestions.length != 0){
-      let decodedCategory = ""
-      let decodedQuestion = ""
-      let decodedCorrect = "" 
-      let decodedIncorrect = "" 
-      let answersArray =[]
-      for (let i = 0; i < fetchQuestions.length; i++) {
-        const questionToDecode = fetchQuestions[i];
-        decodedCategory = (decodeURI(questionToDecode.category)).split('\%')[0]
-        decodedQuestion = decodeURI(questionToDecode.question)
-        decodedCorrect = decodeURI(questionToDecode.correct_answer)
-        decodedIncorrect =  decodeURI(questionToDecode.incorrect_answers)
-        answersArray = decodedIncorrect.split(",")
-        let randomIndex = Math.floor(Math.random() * answersArray.length)
-        answersArray.splice(randomIndex,0, decodedCorrect)
-        
-          const questionsWithIdAndDecoded = fetchQuestions.map(question =>{
-            return {
-              ...question,
-              category: decodedCategory,
-              question: decodedQuestion,
-            }
-          })
-      setAllQuestions(questionsWithIdAndDecoded)
-      console.log(questionsWithIdAndDecoded)
-
-      //all questions are the same like this
-      }
+  const decoding = function(){
+    let entireArrayOfAnswers = []
+    for (let i = 0; i < allQuestions.length; i++) {
+      const q = allQuestions[i];
+      let answersArray = [...q.incorrect_answers]
+      let randomIndex = Math.floor(Math.random() * answersArray.length)
+      answersArray.splice(randomIndex, 0, q.correct_answer)
+      entireArrayOfAnswers.push(answersArray)
     }
+    const newArray = allQuestions.map(e=>{
+      let i = allQuestions.indexOf(e)
+      return {
+        ...e,
+        answersArray: entireArrayOfAnswers[i]
+      }
+    })
+    //how can you update aq if this function is dependent upen it? EFFECT  
+    // (allQuestions)
   }
-     
-
-     
+    
+  
+      
 //end new code
 
-  React.useEffect(()=>{
-    formCheck()
-  },[allQuestions])
+  // React.useEffect(()=>{
+  //   formCheck()
+  // },[allQuestions])
 
-  function formCheck(){
-    if(allQuestions.length !=0){
-      for (let i = 0; i < allQuestions.length; i++) {
-        const element = allQuestions[i].id;
-        setFormData(prevFormData => {
-          return{
-            ...prevFormData,
-            [element]: ""
-          }
-        })
-      }
-    }
-  }
+  // function formCheck(){
+  //   if(allQuestions.length !=0){
+  //     for (let i = 0; i < allQuestions.length; i++) {
+  //       const element = allQuestions[i].id;
+  //       setFormData(prevFormData => {
+  //         return{
+  //           ...prevFormData,
+  //           [element]: ""
+  //         }
+  //       })
+  //     }
+  //   }
+  // }
 
-    function handleChange(e) {
-      const answer = e.target.value
-      const referenceQuestion = e.target.name
+  //   function handleChange(e) {
+  //     const answer = e.target.value
+  //     const referenceQuestion = e.target.name
 
-      if(formData[referenceQuestion] != null){
-        setFormData(prevFormData => {
-              return {
-                ...prevFormData,
-                [referenceQuestion] : answer
-              }
-            })
-            console.log(formData)
-      }
-    }  
+  //     if(formData[referenceQuestion] != null){
+  //       setFormData(prevFormData => {
+  //             return {
+  //               ...prevFormData,
+  //               [referenceQuestion] : answer
+  //             }
+  //           })
+  //           console.log(formData)
+  //     }
+  //   }  
 
-  const question = allQuestions.map(item => {
-    return (
-      <Question 
-        {...item}
-        key={item.id}
-        onChange={handleChange}
-        formData = {formData}
-      />
-    )
-  })        
+  // const question = allQuestions.map(item => {
+  //   return (
+  //     <Question 
+  //       {...item}
+  //       key={item.id}
+  //       onChange={handleChange}
+  //       formData = {formData}
+  //     />
+  //   )
+  // })        
 
   return (
     <div className="App">
       <form>
-        {question}
+        h
+        {/* {question} */}
         {/* <button onClick={handleSubmission}>Submit</button> */}
       </form>
     </div>
