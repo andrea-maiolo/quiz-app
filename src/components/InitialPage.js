899c7b907be531b64632ede6b9a55a46b3df6853
andrea-maiolo <maiolo.m.2@gmail.com>
2022-03-25 16:29:08 +0100

    initial page styled

--- src/components/InitialPage.js
+++ src/components/InitialPage.js
@@ -3,9 +3,9 @@ import React from 'react';
 function InitialPage(props){
     return(
         <div className="initial-page">
-            <h1>Quiz time!</h1>
-            <p>This is a project created in react. Enjoy the quiz!</p>
-            <button onClick={props.handleDisplay}>Start quiz</button>
+            <h1 id="initialTitle">Quiz time!</h1>
+            <p id="initialDescription">This is a project created in react. Enjoy the quiz!</p>
+            <button id="initialButton" onClick={props.handleDisplay}>Start quiz</button>
         </div>
     )
 }
